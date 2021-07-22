047425c978176e7bee35d68439893f7b5949c5c7
Jeong, HyeonGeon <hg1001@dawoncomputing.com>
2021-07-22 09:00:35 +0900

    learn

---
+++ bin/0fristStep.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+a= np.arange(10)
+
+print(a)
+
+""" 환경을 변경하고 싶다면 컨트롤 쉬프트 p를 누른후 select interpreter를 검색하고 python interpreter를 클릭한뒤 버전을 선택하면 된다. 
+    가상환경을 만들때 주의할점은 python = 3.8 이런식으로 추가해주어야 python이 설치되고 interpreter를 할 수 있다. interpreter를 설정후
+    cmd창으로 터미널을 확인하면 py38(내가 만든 가상환경) 이 뜨는것을 확인가능!"""
+
+
+print("테스트 실험 완료 기초강좌 start")

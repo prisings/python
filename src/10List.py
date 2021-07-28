1914cf382f88eb9c6b8e504d505d2a4a1b36d1d4
Jeong, HyeonGeon <hg1001@dawoncomputing.com>
2021-07-29 08:42:57 +0900

    learn

---
+++ src/10List.py
@@ -0,0 +1,16 @@
+#list
+list = []
+li = list()
+
+li=[1,2,3]
+
+#인덱스는 똑같음
+#li.append 그냥 맨뒤에 추가
+#li.remove 해당값 삭제
+#li.insert(a,b) a배열에 b추가
+#체크하기 'a'in li
+#li.count(a)
+#len (length)를 말한다.
+#역순 revers()
+#sort() 오름
+#sort(reverse=True) 내림

1d5539c65368199f2357fa44f5e0d948fcad8423
Jeong, HyeonGeon <hg1001@dawoncomputing.com>
2021-07-30 08:40:15 +0900

    learn

---
+++ bin/13dictionary.py
@@ -0,0 +1,13 @@
+dic = {}
+dic = dict()
+
+#특징  a라는 키에 들은 값은 b다 
+dic = {'a':1}
+
+#del dic[a] 삭제
+#dic.clear() 전체삭제
+#key in dic 확인
+#len(dic) 
+#dic.keys() 모든키
+#dic.values() 모든값
+#dic.items() 모든 순서쌍

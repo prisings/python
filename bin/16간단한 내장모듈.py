191d00daa7a5adaa5d83dfa9ce6bbbf008177d55
Jeong, HyeonGeon <hg1001@dawoncomputing.com>
2021-08-04 08:38:30 +0900

    LEARN

---
+++ bin/16간단한 내장모듈.py
@@ -0,0 +1,26 @@
+
+import math
+#math
+#math.ceil
+#math.floor
+#math.factorial
+#math.sqrt
+#math.pi
+
+s= 1
+li = [1,2,3,4,5]
+t =0
+for i in range(5):
+    t = t+s
+
+print(t)
+
+
+
+import random
+#random
+#random.randint(a,b)
+#random.random()
+#random.choice
+#random.sample
+#random.shuffle
---
+++ src/17약수와배수.py
@@ -0,0 +1,6 @@
+#약수 배수구하기
+n = int(input("num : "))
+
+for i in range(1,101):
+    if n%i ==0: #n과 i 를 바꾸면 배수
+        pass
